09b2987e05b9466b4f0e00641e53dd53192002aa
cpatsalidis <christos.patsalidis20@imperial.ac.uk>
2025-01-19 16:21:42 +0200

    setup mongo connection

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 const express = require('express');
+const { default: mongoose } = require('mongoose');
 const app = express();
 const path = require('path');
+
+mongoose.connect('mongodb://localhost:27017/shopApp', { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => {
+        console.log('Connected to MongoDB');
+    })
+    .catch(err => {
+        console.error('Error connecting to MongoDB', err);
+    });
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
